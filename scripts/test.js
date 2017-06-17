14b1a62ede389115c9e4c47e00a8fdc3bbffb430
kimxogus <kgyoo8232@gmail.com>
2017-06-17 14:01:54 +0900

    fix test error

--- scripts/test.js
+++ scripts/test.js
@@ -6,19 +6,19 @@ if(process.versions.node >= '4') {
   jest.run();
 } else {
   var assert = require('assert');
-  var injectEnv = require('inject-env');
+  var injectEnv = require('../index');
 
   // should extract env variable
-  assert.equal(injectEnv('${PWD}'), process.cwd());
-  assert.equal(injectEnv('${PWD}/${PWD}'), process.cwd() + '/' + process.cwd());
-  assert.equal(injectEnv('${PWD}/file'), process.cwd() + '/file');
+  assert.equal(injectEnv('${PWD}'), process.cwd(), 'should extract env variable');
+  assert.equal(injectEnv('${PWD}/${PWD}'), process.cwd() + '/' + process.cwd(), 'should extract env variable');
+  assert.equal(injectEnv('${PWD}/file'), process.cwd() + '/file', 'should extract env variable');
 
   // should return default value if env does not exists
-  assert.equal(injectEnv('${NOT_EXISTS:default}'), 'default');
-  assert.equal(injectEnv('${NOT_EXISTS:default}/${NO:no}'), 'default/no');
-  assert.equal(injectEnv('${NOT_EXISTS:default}/file'), 'default/file');
+  assert.equal(injectEnv('${NOT_EXISTS:default}'), 'default', 'should return default value if env does not exists');
+  assert.equal(injectEnv('${NOT_EXISTS:default}/${NO:no}'), 'default/no', 'should return default value if env does not exists');
+  assert.equal(injectEnv('${NOT_EXISTS:default}/file'), 'default/file', 'should return default value if env does not exists');
 
-  // 'does not work without '{' and '}' characters
-  assert.equal(injectEnv('$NODE_ENV'), '$NODE_ENV');
-  assert.equal(injectEnv('$NODE_ENV:default'), '$NODE_ENV:default');
+  // does not work without '{' and '}' characters
+  assert.equal(injectEnv('$NODE_ENV'), '$NODE_ENV', 'does not work without \'{\' and \'}\' characters');
+  assert.equal(injectEnv('$NODE_ENV:default'), '$NODE_ENV:default', 'does not work without \'{\' and \'}\' characters');
 }
