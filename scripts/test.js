e198317f14f80081b10a3b5aaa615276f3819c24
kimxogus <kgyoo8232@gmail.com>
2017-06-17 14:14:36 +0900

    remove jest

--- scripts/test.js
+++ scripts/test.js
@@ -1,24 +1,18 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
-if(process.versions.node >= '4') {
-  var jest = require('jest');
+var assert = require('assert');
+var injectEnv = require('../index');
 
-  jest.run();
-} else {
-  var assert = require('assert');
-  var injectEnv = require('../index');
+// should extract env variable
+assert.equal(injectEnv('${PWD}'), process.cwd(), 'should extract env variable');
+assert.equal(injectEnv('${PWD}/${PWD}'), process.cwd() + '/' + process.cwd(), 'should extract env variable');
+assert.equal(injectEnv('${PWD}/file'), process.cwd() + '/file', 'should extract env variable');
 
-  // should extract env variable
-  assert.equal(injectEnv('${PWD}'), process.cwd(), 'should extract env variable');
-  assert.equal(injectEnv('${PWD}/${PWD}'), process.cwd() + '/' + process.cwd(), 'should extract env variable');
-  assert.equal(injectEnv('${PWD}/file'), process.cwd() + '/file', 'should extract env variable');
+// should return default value if env does not exists
+assert.equal(injectEnv('${NOT_EXISTS:default}'), 'default', 'should return default value if env does not exists');
+assert.equal(injectEnv('${NOT_EXISTS:default}/${NO:no}'), 'default/no', 'should return default value if env does not exists');
+assert.equal(injectEnv('${NOT_EXISTS:default}/file'), 'default/file', 'should return default value if env does not exists');
 
-  // should return default value if env does not exists
-  assert.equal(injectEnv('${NOT_EXISTS:default}'), 'default', 'should return default value if env does not exists');
-  assert.equal(injectEnv('${NOT_EXISTS:default}/${NO:no}'), 'default/no', 'should return default value if env does not exists');
-  assert.equal(injectEnv('${NOT_EXISTS:default}/file'), 'default/file', 'should return default value if env does not exists');
-
-  // does not work without '{' and '}' characters
-  assert.equal(injectEnv('$NODE_ENV'), '$NODE_ENV', 'does not work without \'{\' and \'}\' characters');
-  assert.equal(injectEnv('$NODE_ENV:default'), '$NODE_ENV:default', 'does not work without \'{\' and \'}\' characters');
-}
+// does not work without '{' and '}' characters
+assert.equal(injectEnv('$NODE_ENV'), '$NODE_ENV', 'does not work without \'{\' and \'}\' characters');
+assert.equal(injectEnv('$NODE_ENV:default'), '$NODE_ENV:default', 'does not work without \'{\' and \'}\' characters');
