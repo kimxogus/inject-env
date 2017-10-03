0d472f59b0beffb08c1563efca26ba1f39503228
kimxogus <kgyoo8232@gmail.com>
2017-10-03 22:05:27 +0900

    add option parameter

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,12 @@ declare namespace injectEnv {
     export function getEnvVar(name: string, defaultValue: ?string, env: ?object): string;
 }
 
-declare function injectEnv (s: string, env: ?object): string;
-declare function injectEnv <T>(a: Array<T>, env: ?object): array<T>;
-declare function injectEnv (o: object, env: ?object): object;
+interface option {
+  defaultValue: ?(string | number),
+  env: ?object,
+}
+
+declare function injectEnv (s: string, option: ?option): string;
+declare function injectEnv <T>(a: Array<T>, option: ?option): array<T>;
+declare function injectEnv (o: object, option: ?option): object;
 export = injectEnv;
--- index.js
+++ index.js
@@ -10,19 +10,22 @@ function getEnvVar(name, defaultValue, env) {
 
 exports.getEnvVar = getEnvVar;
 
-function injectEnv(input, env) {
+function injectEnv(input, option) {
   if(typeof input === 'string') {
     while (regexp.test(input)) {
       var r = RegExp.$1;
       var split = r.split(':');
-      input = input.replace('${' + r + '}', getEnvVar(split[0], split[1], env));
+      input = input.replace(
+        '${' + r + '}',
+         getEnvVar(split[0], split[1] || option.defaultValue || '', option.env)
+      );
     }
     return input;
   } else if(Array.isArray(input)) {
-    return input.map(i => injectEnv(i, env));
+    return input.map(i => injectEnv(i, option));
   } else if(isPlainObject(input)) {
     return Object.keys(input).reduce((a, b) => {
-      a[b] = injectEnv(input[b], env);
+      a[b] = injectEnv(input[b], option);
       return a;
     }, {});
   }
